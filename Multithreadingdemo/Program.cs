9ce905844457e35998a50231b982c447db2a35f3
Moneshwari Panchal <moneshwaripanchal@gmail.com>
2022-06-08 16:08:24 +0530

    commit

--- Multithreadingdemo/Program.cs
+++ Multithreadingdemo/Program.cs
@@ -16,6 +16,7 @@ namespace Multithreadingdemo
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine(i);
+                //Thread.Sleep(2000);
             }
         }
     }
@@ -26,10 +27,17 @@ namespace Multithreadingdemo
             Multithreadingd mt = new Multithreadingd();
             Thread t1 = new Thread(new ThreadStart(mt.Run));
             Thread t2 = new Thread(new ThreadStart(mt.Run));
+            Thread t3 = new Thread(new ThreadStart(mt.Run));
             t1.Name = "t1 thread";
             t2.Name = "t2 thread";
+            t3.Name = "t3 thread";
+            t1.Priority = ThreadPriority.Lowest;
+            t2.Priority = ThreadPriority.AboveNormal;
+            t3.Priority = ThreadPriority.Highest;
             t1.Start();
+            t1.Join();//block other thread and excute t1 complete it's task
             t2.Start();
+            t3.Start();
             Console.ReadLine();
         }
     }
